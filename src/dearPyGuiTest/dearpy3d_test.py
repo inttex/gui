4b0edf03f64fed55fbe8dc4e29ecdb2ddb50963b
Jonas Hiller <jonas_hiller@gmx.net>
2023-03-10 22:58:03 +0100

    attempt to make it more flexible

--- src/dearPyGuiTest/dearpy3d_test.py
+++ src/dearPyGuiTest/dearpy3d_test.py
@@ -1,3 +1,5 @@
+import itertools
+
 import dearpygui.dearpygui as dpg
 import math
 import numpy as np
@@ -15,8 +17,6 @@ class Camera:
         self.view_to = pos
         self.eye_pos = dpg.mvVec4(50.0, 50.0, 50.0, 1.0)
         self.eye_up, self.eye_right = self.get_up_right(up=np.array([0, 1, 0]))
-
-        dpg.create_lookat_matrix([self.eye_pos[_] for _ in range(4)], [self.view_to[_] for _ in range(4)], self.eye_up)
 
         self.rotating = False
         self.moving = False
@@ -75,10 +75,13 @@ class Camera:
         dpg.set_value("Camera Z", f'{self.eye_pos[2]:0.2f}')
 
     def view_matrix(self):
-        return dpg.create_lookat_matrix([self.eye_pos[_] for _ in range(4)], [self.view_to[_] for _ in range(4)], self.eye_up)
+        return dpg.create_lookat_matrix([self.eye_pos[_] for _ in range(4)],
+                                        [self.view_to[_] for _ in range(4)],
+                                        self.eye_up)
 
     def projection_matrix(self, width, height):
-        return dpg.create_perspective_matrix(math.pi * self.field_of_view / 180.0, width / height, self.nearClip,
+        return dpg.create_perspective_matrix(math.pi * self.field_of_view / 180.0, width / height,
+                                             self.nearClip,
                                              self.farClip)
 
     def move(self, dx, dy):
@@ -100,7 +103,7 @@ class Camera:
     def rotate(self, dx, dy):
         rot_around_y = dx * self.rotation_speed
         rot_around_x = dy * self.rotation_speed
-        # self.eye_pos
+
         self.eye_up, self.eye_right = self.get_up_right()
         self.eye_pos = dpg.create_rotation_matrix(rot_around_x, self.eye_right) * \
                        dpg.create_rotation_matrix(rot_around_y, self.eye_up) * \
@@ -137,23 +140,73 @@ class Camera:
                                  callback=lambda s, a: self._set_field_of_view(a))
 
 
+def getxyz_sphere(r, theta, phi):
+    y = r * np.sin(theta)
+    radius = r * np.cos(theta)
+    x = radius * np.cos(phi)
+    z = radius * np.sin(phi)
+    return [x, y, z]
+
+
+def get_4_pt_groups_sphere(radius, nb_thetas, nb_phis):
+    d_theta = np.pi / nb_thetas
+    d_phi = 2 * np.pi / nb_phis
+    groups = []
+    for theta in np.linspace(-np.pi / 2, np.pi / 2, nb_thetas, endpoint=False):
+        for phi in np.linspace(0, 2 * np.pi, nb_phis, endpoint=False):
+            pts4 = [
+                getxyz_sphere(radius, theta, phi + d_phi),
+                getxyz_sphere(radius, theta + d_theta, phi + d_phi),
+                getxyz_sphere(radius, theta + d_theta, phi),
+                getxyz_sphere(radius, theta, phi),
+            ]
+            groups.append(pts4)
+    return groups
+
+
+def get_4_pts_groups_cylinder_2pt(radius, pt1, pt2, nb_thetas):
+    get_4_pts_groups_cylinder(radius, 1, nb_thetas)
+    transform_pts
+
+
+def get_4_pts_groups_cylinder(radius, height, nb_thetas):
+    todo
+
+
+def get_4_pts_groups_polyline(pts, radius, nb_corners=6):
+    groups = []
+    for pt1, pt2 in itertools.pairwise(pts):
+        for corner:
+            get_4_pts_groups_cylinder_2pt(radius, pt1, pt2, nb_corners)
+
+
+def draw_4_pt_groups(groups, color, fill, thickness):
+    for pts4 in groups:
+        dpg.draw_triangle(pts4[0], pts4[1], pts4[2], color=color,
+                          fill=fill, thickness=thickness)
+        dpg.draw_triangle(pts4[2], pts4[3], pts4[0], color=color,
+                          fill=fill, thickness=thickness)
+
+
 class Sphere:
-    def __init__(self, pos=(0, 0, 0), radius=1, alpha=255):
+    def __init__(self, pos=(0, 0, 0), radius=1, alpha=255, color=(255, 255, 255, 10)):
         self.radius = radius
         self.alpha = alpha
-        self.node = dpg.generate_uuid()
+
+        self.sphere_solid = dpg.generate_uuid()
         self.node2 = dpg.generate_uuid()
+
+        self.nodes = []
+        self.nodes.append(self.sphere_solid)
+        self.nodes.append(self.node2)
+
         self.layer = dpg.generate_uuid()
         self.pos = pos
         self.rot = [0, 0, 0]
         self.scale = [1.0, 1.0, 1.0]
+        self.color = color
 
-    def getxyz(cls, r, theta, phi):
-        y = r * np.sin(theta)
-        radius = r * np.cos(theta)
-        x = radius * np.cos(phi)
-        z = radius * np.sin(phi)
-        return [x, y, z]
+        self.groups_sphere = get_4_pt_groups_sphere(radius=self.radius, nb_thetas=32, nb_phis=32)
 
     def submit(self, layer=None):
         if layer is None:
@@ -164,39 +217,60 @@ class Sphere:
             dpg.push_container_stack(layer)
             self.layer = layer
 
-        with dpg.draw_node(tag=self.node):
-            nb_thetas = 16
-            nb_phis = 16
-            d_theta = np.pi / nb_thetas
-            d_phi = 2 * np.pi / nb_phis
-            for theta in np.linspace(-np.pi / 2, np.pi / 2, nb_thetas, endpoint=False):
-                for phi in np.linspace(0, 2 * np.pi, nb_phis, endpoint=False):
-                    pts4 = [
+        with dpg.draw_node(tag=self.sphere_solid):
+            draw_4_pt_groups(groups=self.groups_sphere, color=[255, 255, 255, 10],
+                             fill=self.color, thickness=1)
 
-                        self.getxyz(self.radius, theta, phi + d_phi),
-                        self.getxyz(self.radius, theta + d_theta, phi + d_phi),
-                        self.getxyz(self.radius, theta + d_theta, phi),
-                        self.getxyz(self.radius, theta, phi),
-                    ]
-                    dpg.draw_triangle(pts4[0], pts4[1], pts4[2], color=[255, 255, 255, 10],
-                                      fill=[150, 150, 150, self.alpha], thickness=1)
-                    dpg.draw_triangle(pts4[2], pts4[3], pts4[0], color=[255, 255, 255, 10],
-                                      fill=[150, 150, 150, self.alpha], thickness=1)
+            draw_quarter_lines = False
+            if draw_quarter_lines:
+                thetas = np.linspace(-np.pi / 250, np.pi / 250, 2, endpoint=False)
+                d_theta = thetas[1] - thetas[0]
+                for theta in thetas:
+                    for phi in np.linspace(0, 2 * np.pi, nb_phis, endpoint=False):
+                        pts4 = [
+                            self.getxyz(self.radius, theta, phi + d_phi),
+                            self.getxyz(self.radius, theta + d_theta, phi + d_phi),
+                            self.getxyz(self.radius, theta + d_theta, phi),
+                            self.getxyz(self.radius, theta, phi),
+                        ]
+                        dpg.draw_triangle(pts4[0], pts4[1], pts4[2], color=self.color,
+                                          fill=[200, 50, 50, self.alpha], thickness=1)
+                        dpg.draw_triangle(pts4[2], pts4[3], pts4[0], color=[255, 0, 255, 255],
+                                          fill=[200, 80, 50, self.alpha], thickness=1)
 
+                        # flip x and y axis
+                        pts4b = [[-y, x, z] for x, y, z in pts4]
+                        dpg.draw_triangle(pts4b[0], pts4b[1], pts4b[2], color=[50, 50, 200, self.alpha],
+                                          fill=[50, 50, 200, self.alpha], thickness=1)
+                        dpg.draw_triangle(pts4b[2], pts4b[3], pts4b[0], color=[50, 50, 200, self.alpha],
+                                          fill=[50, 50, 200, self.alpha], thickness=1)
+
+                        # flip x and y axis
+                        pts4b = [[x, -z, y] for x, y, z in pts4]
+                        dpg.draw_triangle(pts4b[0], pts4b[1], pts4b[2], color=[50, 200, 50, self.alpha],
+                                          fill=[50, 200, 50, self.alpha], thickness=1)
+                        dpg.draw_triangle(pts4b[2], pts4b[3], pts4b[0], color=[50, 200, 50, self.alpha],
+                                          fill=[50, 200, 50, self.alpha], thickness=1)
+
+        nb_thetas = 32
+        nb_phis = 32
         with dpg.draw_node(tag=self.node2):
             theta = 0
+            d_theta = np.pi / nb_thetas
+            d_phi = np.pi / nb_phis
             for phi in np.linspace(0, 2 * np.pi, nb_phis, endpoint=False):
-                dpg.draw_line(self.getxyz(self.radius, theta, phi), self.getxyz(self.radius, theta, phi + d_phi),
-                              color=(255, 0, 0))
-
+                dpg.draw_line(getxyz_sphere(self.radius, theta, phi), getxyz_sphere(self.radius, theta, phi + d_phi),
+                              color=(255, 0, 0, 100))
             for phi in [0, np.pi]:
                 for theta in np.linspace(-np.pi / 2, np.pi / 2, nb_thetas, endpoint=False):
-                    dpg.draw_line(self.getxyz(self.radius, theta, phi), self.getxyz(self.radius, theta + d_theta, phi),
-                                  color=(0, 255, 0))
+                    dpg.draw_line(getxyz_sphere(self.radius, theta, phi),
+                                  getxyz_sphere(self.radius, theta + d_theta, phi),
+                                  color=(0, 255, 0, 100))
             for phi in [np.pi / 2, -np.pi / 2]:
                 for theta in np.linspace(-np.pi / 2, np.pi / 2, nb_thetas, endpoint=False):
-                    dpg.draw_line(self.getxyz(self.radius, theta, phi), self.getxyz(self.radius, theta + d_theta, phi),
-                                  color=(0, 0, 255))
+                    dpg.draw_line(getxyz_sphere(self.radius, theta, phi),
+                                  getxyz_sphere(self.radius, theta + d_theta, phi),
+                                  color=(0, 0, 255, 100))
         dpg.pop_container_stack()
 
     def update(self, projection, view):
@@ -205,17 +279,19 @@ class Sphere:
                 * dpg.create_rotation_matrix(self.rot[1], [0, 1, 0]) \
                 * dpg.create_rotation_matrix(self.rot[2], [0, 0, 1]) \
                 * dpg.create_scale_matrix(self.scale)
-        dpg.apply_transform(self.node, projection * view * model)
-        dpg.apply_transform(self.node2, projection * view * model)
+
+        for node in self.nodes:
+            dpg.apply_transform(node, projection * view * model)
+        # dpg.apply_transform(self.node2, projection * view * model)
 
     def update_clip_space(self, top_left_x, top_left_y, width, height, min_depth, max_depth):
         dpg.set_clip_space(self.layer, top_left_x, top_left_y, width, height, min_depth, max_depth)
 
 
 camera = Camera(dpg.mvVec4(0.0, 0.0, 0.0, 1.0), 0.0, 0.0)
-sphere = Sphere(radius=10, alpha=255)
+sphere = Sphere(radius=10, alpha=255, color=[100, 100, 100, 255])
 
-sphere2 = Sphere(pos=[0, 4, 6, ], radius=5, alpha=255)
+# sphere2 = Sphere(pos=[0, 4, 6, ], radius=5, alpha=255, color=[255, 155, 155, 255])
 camera.show_controls()
 
 dpg.set_viewport_resize_callback(lambda: camera.mark_dirty())
@@ -223,7 +299,7 @@ rect = dpg.generate_uuid()
 
 with dpg.viewport_drawlist(front=False):
     sphere.submit()
-    sphere2.submit(sphere.layer)
+    # sphere2.submit(sphere.layer)
 
     dpg.draw_rectangle((0, 0), (10, 10), tag=rect)
     with dpg.draw_layer(perspective_divide=True, tag="gizmo_layer"):
@@ -254,11 +330,11 @@ with dpg.handler_registry(tag="__demo_keyboard_handler"):
 dpg.show_viewport()
 while dpg.is_dearpygui_running():
 
-    if camera.dirty :
+    if camera.dirty:
         width = dpg.get_viewport_client_width()
         height = dpg.get_viewport_client_height()
         sphere.update_clip_space(width / 4, height / 4, width / 2, height / 2.0, -1.0, 1.0)
-        sphere2.update_clip_space(width / 4, height / 4, width / 2, height / 2.0, -1.0, 1.0)
+        # sphere2.update_clip_space(width / 4, height / 4, width / 2, height / 2.0, -1.0, 1.0)
         dpg.configure_item(rect, pmin=(width / 4, height / 4), pmax=(0.75 * width, 0.75 * height))
         view = camera.view_matrix()
         projection = camera.projection_matrix(width / 2, height / 2)
@@ -267,10 +343,9 @@ while dpg.is_dearpygui_running():
         dpg.set_clip_space("gizmo_layer", width / 4, height / 4, width / 2, height / 2.0, -1.0, 1.0)
 
         sphere.update(projection, view)
-        sphere2.update(projection, view)
+        # sphere2.update(projection, view)
 
         camera.dirty = False
-
 
     dpg.render_dearpygui_frame()
 
